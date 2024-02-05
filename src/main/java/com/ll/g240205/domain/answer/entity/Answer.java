860867cde643d110dc033f0093ede009a3a43a85
seoyeon <tus0926@naver.com>
2024-02-05 16:31:29 +0900

    Answer Entity

---
+++ src/main/java/com/ll/g240205/domain/answer/entity/Answer.java
@@ -0,0 +1,23 @@
+package com.ll.g240205.domain.answer.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    // Question 연결해야 함
+}
