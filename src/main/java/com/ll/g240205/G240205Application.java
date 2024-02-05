416a280309d8559699bc5889f9b02e81fbf0bfc3
seoyeon <tus0926@naver.com>
2024-02-05 13:10:05 +0900

    start

---
+++ src/main/java/com/ll/g240205/G240205Application.java
@@ -0,0 +1,13 @@
+package com.ll.g240205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G240205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G240205Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/g240205/G240205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.g240205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G240205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
