1d2fa7321a7aae7e6727f1166ae5060e753282de
seoyeon <tus0926@naver.com>
2024-02-05 17:34:08 +0900

    entity 수정

--- src/main/java/com/ll/g240205/domain/question/entity/Question.java
+++ src/main/java/com/ll/g240205/domain/question/entity/Question.java
@@ -14,6 +14,9 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(length = 200)
+    private String subject;
+
     @Column(columnDefinition = "TEXT")
     private String content;
 
